8803b9985b0720ed8e6363658e5d6879a65d499d
KinectTeam <Shared Account -- Check Blame>
2013-11-03 19:47:33 -0700

    Improve bug spawning and movement. Squash function. Splat graphics. Formatting.

--- src/main/java/edu/mines/johkelly/modules/bugsquash/BugSquashModule.java
+++ src/main/java/edu/mines/johkelly/modules/bugsquash/BugSquashModule.java
@@ -2,28 +2,23 @@ package edu.mines.johkelly.modules.bugsquash;
 
 import edu.mines.acmX.exhibit.module_management.modules.ProcessingModule;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 import processing.core.PImage;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-/**
- * Hello world!
- *
- */
-public class BugSquashModule extends ProcessingModule
-{
+public class BugSquashModule extends ProcessingModule {
     private static final int extMargin = 300;
     private static final int expFPS = 30;
 
-    private Logger log = LogManager.getLogger(BugSquashModule.class);
+    // private Logger log = LogManager.getLogger(BugSquashModule.class);
 
     private List<Bug> bugs = new ArrayList<Bug>();
+    private final List<Splat> splats = new ArrayList<Splat>();
     private PImage bugSprite;
 
-    int c = 0;
+    private int framesSinceLastBug = 0;
 
     @Override
     public void setup() {
@@ -33,56 +28,18 @@ public class BugSquashModule extends ProcessingModule
     }
 
     public void update() {
-        c++;
-        if(c%(expFPS)==0){
+        framesSinceLastBug++;
+        // Spawn one bug every second
+        if (framesSinceLastBug % (expFPS) == 0) {
             spawnBug();
-            c = 0;
+            framesSinceLastBug = 0;
         }
-        List<Bug> deadBugs = new ArrayList<Bug>();
-        for(Bug b : bugs){
+        // Update each bug's position and remove it if it is too far offscreen
+        for (Iterator<Bug> iter = bugs.iterator(); iter.hasNext(); /**/) {
+            Bug b = iter.next();
             b.update();
-            if(!inBounds(b)){
-                deadBugs.add(b);
-            }
-        }
-        for(Bug b : deadBugs){
-            bugs.remove(b);
-        }
-    }
-
-    private boolean inBounds(Bug b){
-        return b.x > 0-extMargin && b.y > 0-extMargin && b.x < width+extMargin && b.y < height+extMargin;
-    }
-
-    private void spawnBug(){
-        int x = (int) (random(1) * (width));
-        int y = (int) (random(1) * (height));
-        float heading = (float) (random(1) * Math.PI / 2);
-        // random boundaries are irrelevant -- just a convenient way to do 50/50 chance
-        // On the vertical rails
-        if(random(1) > .5){
-            // On the right
-            if(random(1) > .5){
-                float rot = (float) (heading - 3 * Math.PI / 4);
-                bugs.add(new Bug(width+extMargin, y, rot, bugSprite));
-            }
-            // On the left
-            else{
-                float rot = (float) (heading + Math.PI / 4);
-                bugs.add(new Bug(-extMargin, y, rot, bugSprite));
-            }
-        }
-        // On the horizontal rails
-        else{
-            // On the bottom
-            if(random(1) > .5){
-                float rot = (float) (heading - Math.PI / 4);
-                bugs.add(new Bug(x, height+extMargin, rot, bugSprite));
-            }
-            // On the top
-            else{
-                float rot = (float) (heading + 3 * Math.PI / 4);
-                bugs.add(new Bug(x, -extMargin, rot, bugSprite));
+            if (!inBounds(b)) {
+                iter.remove();
             }
         }
     }
@@ -91,8 +48,83 @@ public class BugSquashModule extends ProcessingModule
     public void draw() {
         background(color(256, 256, 256));
         update();
-        for(Bug b : bugs){
+        for (Bug b : bugs) {
             b.draw(this);
+        }
+        synchronized (splats) {
+            for (Splat s : splats) {
+                s.draw(this);
+            }
+        }
+        fill(color(0));
+    }
+
+    private boolean inBounds(Bug b) {
+        return b.x > 0 - extMargin &&
+                b.y > 0 - extMargin &&
+                b.x < width + extMargin &&
+                b.y < height + extMargin;
+    }
+
+    private void spawnBug() {
+        int x = (int) (random(1) * (width));
+        int y = (int) (random(1) * (height));
+        float heading = (float) (random(1) * Math.PI / 2);
+        // random boundaries are irrelevant -- just a convenient way to do 50/50 chance
+        // On the vertical rails
+        if (random(1) > .5) {
+            // On the right
+            if (random(1) > .5) {
+                float rot = (float) (heading - 3 * Math.PI / 4);
+                bugs.add(new Bug(width + extMargin, y, rot, bugSprite));
+            }
+            // On the left
+            else {
+                float rot = (float) (heading + Math.PI / 4);
+                bugs.add(new Bug(-extMargin, y, rot, bugSprite));
+            }
+        }
+        // On the horizontal rails
+        else {
+            // On the bottom
+            if (random(1) > .5) {
+                float rot = (float) (heading - Math.PI / 4);
+                bugs.add(new Bug(x, height + extMargin, rot, bugSprite));
+            }
+            // On the top
+            else {
+                float rot = (float) (heading + 3 * Math.PI / 4);
+                bugs.add(new Bug(x, -extMargin, rot, bugSprite));
+            }
+        }
+    }
+
+    public void squash(int x, int y) {
+        // Test all bugs for collision
+        for (Iterator<Bug> iter = bugs.iterator(); iter.hasNext(); /**/) {
+            Bug b = iter.next();
+            // distance between squash point and bug position
+            double dist = Math.sqrt(Math.pow(b.x - x, 2) + Math.pow(b.y - y, 2));
+            // The height is the larger graphic dimension, so players can "fat finger" a bit off target
+            if (dist < bugSprite.height / 2) {
+                iter.remove();
+                // Spawn a new splat to display
+                String splatGraphicName = "splat" + (int) (random(3) + 1) + ".png";
+                final Splat s = new Splat(b.x, b.y, loadImage(splatGraphicName));
+                splats.add(s);
+                // Schedule synchronized deletion of the graphic 2 seconds later
+                new Timer().schedule(
+                        new TimerTask() {
+                            @Override
+                            public void run() {
+                                synchronized (splats) {
+                                    splats.remove(s);
+                                }
+                            }
+                        },
+                        2000
+                );
+            }
         }
     }
 }
--- src/main/java/edu/mines/johkelly/modules/bugsquash/Splat.java
+++ src/main/java/edu/mines/johkelly/modules/bugsquash/Splat.java
@@ -4,31 +4,20 @@ import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
 
-public class Bug {
-    public int x, y;
-    private float rotRadians;
-    private PImage sprite;
+public class Splat {
+    int x, y;
+    PImage sprite;
 
-    private static final int SPEED = 10;
-
-    public Bug(int x, int y, float rotRadians, PImage sprite) {
+    public Splat(int x, int y, PImage sprite) {
         this.x = x;
         this.y = y;
-        this.rotRadians = rotRadians;
         this.sprite = sprite;
-    }
-
-    public void update() {
-        // 0 radians in Processing coordinates points up, not right
-        x += SPEED * Math.sin(rotRadians);
-        y -= SPEED * Math.cos(rotRadians);
     }
 
     public void draw(PApplet canvas) {
         canvas.imageMode(PConstants.CENTER);
         canvas.pushMatrix();
         canvas.translate(x, y);
-        canvas.rotate(rotRadians);
         canvas.image(sprite, 0, 0);
         canvas.popMatrix();
         canvas.imageMode(PConstants.CORNER);
